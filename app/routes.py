47cc3bc5910c9e60ef2c19d2b7b599b6e61b4cbb
KacperZ96 <65060846+KacperZ96@users.noreply.github.com>
2020-05-28 19:01:23 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -8,25 +8,25 @@ import model
 @app.route('/', methods=['GET','POST'])
 def index():
 
-    # pobierz dane jesli zostaly wyslane
+    
     if request.method == 'POST':
 
-        # pobierz nazwe name z formularza
+        
         name = request.form['name']
 
-        # stworz obiekt Name, z domyslnymi wartosciami
+        
         name_object = db_models.Name(name=name)
 
-        # klasyfikuj imię
+       
         gender_number = model.get_prediction(model_names, name)
 
-        # jesli gender rowna sie 1 to mezczyzna
+        
         if gender_number:
             name_object.gender_predict = 'Male'
         else:
             name_object.gender_predict = 'Female'
 
-        # dodaj do sesji i dodaj do bazy
+        
         db.session.add(name_object)
         db.session.commit()
 
@@ -39,11 +39,11 @@ def index():
 def view_prediction(id):
     name_object = db_models.Name.query.get(id)
 
-    # zapisz informacje czy imie zostało dobrze sklasyfikowane
+    
     if request.method == 'POST':
         name_object.correct = request.form['correct']
 
-    # zapis do bazy
+    
     db.session.commit()
 
     return render_template('result.html', name_object=name_object)
