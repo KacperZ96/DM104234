e845fed5ccdeeb002884ef644855cf4e308f698e
KacperZ96 <65060846+KacperZ96@users.noreply.github.com>
2020-05-28 19:01:57 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import model
 
-# tworzenie aplikacji flask
+
 app = Flask(__name__, template_folder='templates')
 app.config.from_object('config')
 
-# baza danych z SQLALCHEMY
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 Bootstrap(app)
 
-# stworzenie modelu do predykcji
+
 model_names = model.create_model(5)
 
-# import widokow
+
 from app import routes
