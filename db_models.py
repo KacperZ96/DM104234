fe417526478a7998354761a0077b831225beccac
KacperZ96 <65060846+KacperZ96@users.noreply.github.com>
2020-05-28 18:20:16 +0200

    Add files via upload

---
+++ db_models.py
@@ -0,0 +1,8 @@
+from app import db
+
+
+class Name(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, nullable=False)
+    gender_predict = db.Column(db.String(64), index=True, nullable=False, default="N/D")
+    correct = db.Column(db.String(64), index=True, nullable=False, default="N/D")
